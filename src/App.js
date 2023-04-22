4ef2cfa35222bc0f665bd81f47bcd19f344d8f73
Sankaranarayanan Sengunther <shankar.muthusamy4@gmail.com>
2023-04-22 14:55:10 +0530

    divided into comps and started with routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Header from './Header';
+import Post from './Post';
+import { Route, Routes } from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route index element={
+        <main>
+          <Header />
+          <Post />
+          <Post />
+          <Post />
+        </main>
+      } />
+
+      <Route path={'/login'} element={
+        <div>
+          login page
+        </div>
+      } />
+    </Routes>
   );
 }
 
---
+++ src/Post.js
@@ -0,0 +1,22 @@
+export default function Post() {
+    return (
+        <div className="post">
+
+            <div className="image">
+                <img src="https://video-images.vice.com/articles/6435c4de2512482057614f4c/lede/1681246231514-screen-shot-2023-04-11-at-44958-pm.png?crop=1xw:0.8468xh;0xw,0xh&resize=500:*" alt="programmer" />
+            </div>
+
+            <div className="texts">
+                <h2>‘Overemployed’ Hustlers Exploit ChatGPT To Take On Even More Full-Time Jobs
+                </h2>
+                <p className="info">
+                    <a className="author"> David Paszko</a>
+                    <time>2023-01-06 16:45</time>
+                </p>
+                <p className="summary">"ChatGPT does like 80 percent of my job," said one worker. Another is holding the line at four robot-performed jobs. "Five would be overkill," he said.</p>
+            </div>
+
+        </div>
+
+    );
+}
